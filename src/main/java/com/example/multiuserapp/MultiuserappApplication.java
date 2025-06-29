91ec6667d6853a76e960222093725f837901d09d
RsCedric <ced.segu@gmail.com>
2025-06-30 00:10:33 +0200

    Grundstruktur

---
+++ src/main/java/com/example/multiuserapp/MultiuserappApplication.java
@@ -0,0 +1,11 @@
+package com.example.multiuserapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiuserappApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MultiuserappApplication.class, args);
+    }
+} 
